216a1c8ce419114d7f7c088ca74d7e165d848f73
Jack Rohkohl <jhrohkohl@gmail.com>
2025-04-09 22:48:41 -0400

    Cleanup: Remove old api/index.py

--- api/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/api/similar-players', methods=['POST'])
-def find_similar_players_route():
-    data = request.get_json()
-    # Call your find_similar_players logic here
-    return jsonify({"message": "Success!", "received": data})
